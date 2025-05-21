621afabea51009fdfce960c5432cf916d0dea4ab
RosyJu <111301965+RosyJu@users.noreply.github.com>
2025-05-21 16:25:57 +0800

    Add files via upload

---
+++ javascript/body.js
@@ -0,0 +1,130 @@
+// 提交事件
+let topicType = "";
+
+let newTopic = [];
+
+window.onload = function () {
+  init();
+  // 画星星
+  for (var i = 0; i < starCount; i++) {
+    var star = new Star();
+    star.init();
+    star.draw();
+    arr.push(star);
+  }
+  // 画流星
+  for (var i = 0; i < rainCount; i++) {
+    var rain = new MeteorRain();
+    rain.init();
+    rain.draw();
+    rains.push(rain);
+  }
+  playStars();
+  playRains();
+  nextTopic();
+};
+
+function send() {
+  if (topicType == "单选题" || topicType == "多选题") {
+    const list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let num = 0;
+    let aws = "";
+    document.querySelectorAll("div.options input").forEach((item) => {
+      if (item.checked) {
+        if (aws == "") {
+          aws = list[num];
+        } else {
+          aws += "," + list[num];
+        }
+      }
+      num++;
+    });
+    document.querySelectorAll("div.right > span")[0].textContent = `${aws}`;
+    if (aws == newTopic.answer) {
+      document.querySelectorAll("div.answer")[0].setAttribute("bool", "true");
+    } else {
+      document.querySelectorAll("div.answer")[0].setAttribute("bool", "false");
+    }
+  } else {
+    if (document.querySelectorAll("div.options input")[0].checked) {
+        document.querySelectorAll("div.right > span")[0].textContent = "yes"
+      if ("yes" == newTopic.answer) {
+        document.querySelectorAll("div.answer")[0].setAttribute("bool", "true");
+      } else {
+        document
+          .querySelectorAll("div.answer")[0]
+          .setAttribute("bool", "false");
+      }
+    } else if (document.querySelectorAll("div.options input")[1].checked) {
+      document.querySelectorAll("div.right > span")[0].textContent = "no"
+        if ("no" == newTopic.answer) {
+        document.querySelectorAll("div.answer")[0].setAttribute("bool", "true");
+      } else {
+        document
+          .querySelectorAll("div.answer")[0]
+          .setAttribute("bool", "false");
+      }
+    }
+  }
+}
+
+
+
+
+
+function nextTopic() {
+  let rand = Math.floor(Math.random() * topic.length);
+
+  const list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  newTopic = topic[rand];
+
+  document.querySelectorAll("div.topic")[0].textContent = topic[rand].topic;
+  if (list.includes(topic[rand].answer)) {
+    // 单选题处理
+    topicType = "单选题";
+    let opt = ``;
+    for (let key in topic[rand].item) {
+      //   console.log(key)
+      opt += `<input name="radio" id="radio${list[key]}" type="radio" />
+        <div class="item">
+          <label for="radio${list[key]}" onclick="setTimeout(() => {
+  send();
+}, 1);"><span>${topic[rand].item[key]}</span></label>
+        </div>`;
+    }
+    document.querySelectorAll("div.options")[0].innerHTML = opt;
+  } else if (["yes", "no"].includes(topic[rand].answer)) {
+    // 判断题处理
+    topicType = "判断题";
+    // console.log(topic[rand].answer);
+    let opt = ``;
+    for (let key in topic[rand].item) {
+      //   console.log(key)
+      opt += `<input name="radio" id="radio${list[key]}" type="radio" />
+        <div class="item">
+          <label for="radio${list[key]}" onclick="setTimeout(() => {
+  send();
+}, 1);"><span>${topic[rand].item[key]}</span></label>
+        </div>`;
+    }
+    document.querySelectorAll("div.options")[0].innerHTML = opt;
+  } else if (topic[rand].answer.includes(",")) {
+    // 多选题处理
+    topicType = "多选题";
+    // console.log(topic[rand].answer);
+    let opt = ``;
+    for (let key in topic[rand].item) {
+      //   console.log(key)
+      opt += `<input name="radio" id="radio${list[key]}" type="checkbox" />
+        <div class="item">
+          <label for="radio${list[key]}"><span>${topic[rand].item[key]}</span></label>
+        </div>`;
+    }
+    document.querySelectorAll("div.options")[0].innerHTML = opt;
+  }
+  document.querySelectorAll("div.answer")[0].setAttribute("bool", ``);
+  document.querySelectorAll(
+    "div.left > span"
+  )[0].textContent = `${newTopic.answer}`;
+}
